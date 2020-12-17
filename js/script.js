4545cac7b4064d3e8da509a6ecd3a3a0ed47fd37
alex3959 <akmal_kamolov.tj57@bk.ru>
2020-12-17 11:46:55 +0500

    added new changes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// let todo_item = document.querySelectorAll('.todo-item');
+// let buttons_wrap = document.querySelector('.buttons-wrap')
+// for (let i = 0; i < todo_item.length; i++) {
+//     todo_item[i].addEventListener('mouseover', function(){
+//         buttons_wrap.style.display = 'flex';
+//         console.log(todo_item[i], 'mouse over');
+        
+//     })
+//     todo_item[i].addEventListener('mouseleave', function(){
+//         buttons_wrap.style.display = 'none';
+//         console.log(todo_item[i], 'mouse leave');
+//     })
+// }
