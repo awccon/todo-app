d061171cd9b21dea8b4d7f33190c14bfc7f9f91f
alex3959 <akmal_kamolov.tj57@bk.ru>
2020-12-23 16:43:13 +0500

    changed some codes and created new function to remove todoList

--- js/script.js
+++ js/script.js
@@ -1,13 +1,49 @@
-// let todo_item = document.querySelectorAll('.todo-item');
-// let buttons_wrap = document.querySelector('.buttons-wrap')
-// for (let i = 0; i < todo_item.length; i++) {
-//     todo_item[i].addEventListener('mouseover', function(){
-//         buttons_wrap.style.display = 'flex';
-//         console.log(todo_item[i], 'mouse over');
-        
-//     })
-//     todo_item[i].addEventListener('mouseleave', function(){
-//         buttons_wrap.style.display = 'none';
-//         console.log(todo_item[i], 'mouse leave');
-//     })
-// }
+
+new Vue({
+    el: ".todo-wrap",
+    data: {
+        todoList: [{
+            todo: 'Сделать адаптивный дизайн'
+        }],
+        inputValue: '',
+        inputVisible: false,
+        changeBtnWasClicked: false,
+        addBtnWasClicked: false,
+        todoNum: 0
+    },
+
+    methods: {
+        addChangeTodo() {
+            if (this.addBtnWasClicked == true) {
+                let todoCount = this.todoList.length + 1;
+                if (this.inputValue.trim()) {
+                    this.todoList.push({
+                        todo: this.inputValue,
+                    });
+                    this.inputValue = '';
+                }
+                this.inputVisible = false
+                this.addBtnWasClicked = false
+            }
+            if (this.changeBtnWasClicked) {
+                this.todoList[this.todoNum].todo = this.inputValue;
+                this.inputVisible = false;
+                this.inputValue = ''
+                this.changeBtnWasClicked = false
+            }
+        },
+        changeBtn(e) {
+            let todoCount = e - 1
+            this.changeBtnWasClicked = true;
+            this.inputVisible = true;
+            this.inputValue = this.todoList[todoCount].todo;
+            this.todoNum = todoCount;
+        },
+
+        removeTodo(e) {
+            let todoCount = e - 1;
+            this.todoList.splice(todoCount, 1);
+            console.log('delete btn was clicked');
+        }
+    }
+})
