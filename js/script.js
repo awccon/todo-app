4eb40721e6207d662de06e6687c11df6a9112a55
alex3959 <akmal_kamolov.tj57@bk.ru>
2021-01-09 16:38:23 +0500

    todo changed to component

--- js/script.js
+++ js/script.js
@@ -1,6 +1,43 @@
+Vue.component('app-left-side', {
+    template: `
+    <div class="left-side">
+            <h1>Список дел</h1>
+            <h4>Добавляете и редактируйте списки дел</h4>
+            <slot></slot>
+            <button class="add-button" @click="$emit('add')">Добавить
+                дело
+            </button>
+    </div>
+    `
+})
+
+Vue.component('app-todo-list', {
+    props: ['id', 'item'],
+    template: `
+    <div class="todo-item">
+        <span class="todo-count">{{id + 1}}.</span>
+        <div class="todo-label">{{item}}</div>
+        <div class="buttons-wrap">
+            <button class="change-button" @click="$emit('change')">Изменить</button>
+            <button class="remove-button" @click="$emit('remove')">Удалить</button>
+        </div>
+    </div>`,
+});
+
+Vue.component('app-right-side', {
+    props: ['value'],
+    template: `
+    <div class="right-side">
+        <div class="input-block">
+            <h2>Что нужно сделать?</h2>
+            <input type="text" placeholder="Побегать вечером" :value="value" @input="$emit('input', $event.target.value)" @keyup.enter="$emit('save')">
+            <button class="save-button" @click="$emit('save')">СОХРАНИТЬ</button>
+        </div>
+    </div>`
+})
 
 new Vue({
-    el: ".todo-wrap",
+    el: "#todo-wrap",
     data: {
         todoList: [{
             todo: 'Сделать адаптивный дизайн'
@@ -12,16 +49,16 @@ new Vue({
     },
 
     methods: {
+        // Save button
         saveButton() {
             if (this.isEdit == false) {
                 this.addTodo();
-            }
-            else {
+            } else {
                 this.editTodo();
             }
         },
 
-        // Todo save
+        // Todo add
         addTodo() {
             if (this.inputValue.trim()) {
                 this.todoList.push({
@@ -42,9 +79,9 @@ new Vue({
         // remove todo list
         removeTodo(index) {
             this.todoList.splice(index, 1);
-            if(this.isOpen){
-                this.isOpen = false;
+            if (this.isOpen) {
                 this.inputValue = '';
+                this.isOpen = false;
             }
         },
 
